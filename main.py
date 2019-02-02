7aaecf91382ac0ca861683b1406902743115035c
Saeed Rahmani <rahmanidashti@gmail.com>
2019-02-02 21:59:57 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-import numpy as np
-import time
 import scipy.sparse as sparse
 from lightfm import LightFM
-from collections import defaultdict
 
 
 def main():
-    sparse_training_matrix, training_tuples = read_training_data()
-    ground_truth = read_ground_truth()
+    # sparse_training_matrix is user-item interaction matrix.
 
-    start_time = time.time()
-
-    model = LightFM(no_components=30, learning_rate=0.05, loss='bpr')
+    model = LightFM(no_components=30, learning_rate=0.05, loss='bpr', item_pretrain=True, item_pretrain_file='item_embeddings.txt')
     model.fit(sparse_training_matrix, epochs=100)
     URB, URE = model.get_user_representations()
     IRB, IRE = model.get_item_representations()
